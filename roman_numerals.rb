deb195458cd70ad340cfa72a36a0adc5005a53cc
Ziajko <tomasz.ziajko@avl.com>
2016-11-30 21:37:34 +0100

    Pass: test_2.

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -4,6 +4,12 @@ end
 
 class Fixnum
   def to_roman 
-    'I'
+    output = String.new
+    temp = self
+    while temp > 0
+      output.concat('I')
+      temp -= 1
+    end
+    output
   end
 end
