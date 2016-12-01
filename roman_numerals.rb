4718ee333931b4cfed6e5dd3627374ce233739a4
Ziajko <tomasz.ziajko@avl.com>
2016-12-01 18:30:50 +0100

    Pass:  test_27  test_48  test_59  test_93  test_141  test_163  test_402  test_575  test_911  test_1024.

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -13,7 +13,35 @@ class Fixnum
     output = String.new
     
     while (number > 0) do
-      if number >= 9
+    
+      if number > 1000
+        output.concat('M')
+        number -= 1000
+      elsif number > 900
+        output.concat('CM')
+        number -= 900
+      elsif number > 500
+        output.concat('D')
+        number -= 500
+      elsif number > 400
+        output.concat('CD')
+        number -= 400
+      elsif number > 100
+        output.concat('C')
+        number -= 100
+      elsif number > 90
+        output.concat('XC')  
+        number -= 90
+      elsif number > 50
+        output.concat('L')  
+        number -= 50
+      elsif number > 40 
+        output.concat('XL')
+        number -= 40
+      elsif number >= 10
+        output.concat('X')
+        number -= 10
+      elsif number >= 9
         output.concat('IX')
         number -= 9
       elsif number >= 5
